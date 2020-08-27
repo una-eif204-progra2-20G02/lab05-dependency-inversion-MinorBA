fb6dd1b5a147a9e453ac41c1795fcbf835459445
MinorBA <68918550+MinorBA@users.noreply.github.com>
2020-08-27 14:32:55 -0600

    Agregada la classe ISenPayment

---
+++ src/ISendPayment.h
@@ -0,0 +1,15 @@
+//
+// Created by OEM on 27/08/2020.
+//
+
+#ifndef MY_PROJECT_NAME_ISENDPAYMENT_H
+#define MY_PROJECT_NAME_ISENDPAYMENT_H
+
+#endif //MY_PROJECT_NAME_ISENDPAYMENT_H
+#include <string>
+
+class IsendPayment{
+public:
+    virtual std::string sendPayment() const = 0;
+    virtual ~IsendPayment() = 0;
+};
